f9cecd1df36ad3042f220b3b70cd2567ee11d4d5
Alistair Martin <2270211+AliMartin@users.noreply.github.com>
2024-11-24 17:13:13 +0000

    Drupal 11 setup

--- sites/default/settings.local.php
+++ sites/default/settings.local.php
@@ -3,7 +3,9 @@
 $settings['update_free_access'] = FALSE;
 $settings['file_private_path'] = DRUPAL_ROOT . '/private-files';
 $settings['file_temp_path'] = DRUPAL_ROOT . '/tmp';
-
+$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
+#$settings['file_assets_path'] = 'sites/default/files';
+#$settings['file_public_base_url'] = 'https://synfinity.ddev.site/files';
 
 
 $settings['trusted_host_patterns'] = [
@@ -13,5 +15,4 @@ $settings['trusted_host_patterns'] = [
   '^.+\.synfinity\.ddev\.site$',
 ];
 
-$settings['config_sync_directory'] = DRUPAL_ROOT . '/sync';
 
